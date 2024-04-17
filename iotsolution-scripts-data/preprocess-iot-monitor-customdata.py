60982e84bc506ffaf44ad069557bc0707021b7d0
johncerbo <144145795+johncerbo@users.noreply.github.com>
2024-04-17 13:45:56 -0400

    Update preprocess-iot-monitor-customdata.py

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -170,7 +170,7 @@ latlong=lat:long'
      identifier = "IoT device performance and failures"
 
      # if dataage - use:dataage_utcoffset_timetype
-     preprocesslogic='min,max,count,variance,outliers,anomrpob'
+     preprocesslogic='min,max,count,variance,outliers,anomprob'
 
      pathtotmlattrs='oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a'          
      try:
